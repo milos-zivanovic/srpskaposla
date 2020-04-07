ff93fb9ab1e49c8f4dd6ecca3b77d6b86d836423
Milos Zivanovic <milos.zivanovic@msgconsulting.net>
2020-04-07 23:55:43 +0200

    initial commit for jobs app

--- srpskaposla/settings.py
+++ srpskaposla/settings.py
@@ -33,12 +33,18 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    # Default Apps
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    # Third-party Apps
+
+    # My Apps
+    'jobs',
 ]
 
 MIDDLEWARE = [
