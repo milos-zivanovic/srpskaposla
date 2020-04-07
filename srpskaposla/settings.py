277988ce1e93b585e83a1bcc8f6a9ec37e702361
Milos Zivanovic <milos.zivanovic@msgconsulting.net>
2020-04-07 21:59:43 +0200

    implementing postgresql database

--- srpskaposla/settings.py
+++ srpskaposla/settings.py
@@ -76,8 +76,12 @@ WSGI_APPLICATION = 'srpskaposla.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'HOST': os.environ['DB_HOST'],
+        'PORT': os.environ['DB_PORT'],
+        'USER': os.environ['DB_USER'],
+        'PASSWORD': os.environ['DB_PASSWORD'],
+        'NAME': os.environ['DB_NAME'],
     }
 }
 
