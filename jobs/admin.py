9e05f7361ffaca1fcb7b05c280cb5046e417aac2
Milos Zivanovic <milos.zivanovic@msgconsulting.net>
2020-04-08 00:01:57 +0200

    adding models to jobs app

--- jobs/admin.py
+++ jobs/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Company, Job, Source, Search
+
+
+admin.site.register(Company)
+admin.site.register(Job)
+admin.site.register(Source)
+admin.site.register(Search)
--- jobs/models.py
+++ jobs/models.py
@@ -1,3 +1,53 @@
 from django.db import models
 
-# Create your models here.
+
+class Source(models.Model):
+    name = models.CharField(max_length=100)
+    url = models.CharField(max_length=100)
+    first_page = models.CharField(max_length=100)
+    image = models.ImageField(upload_to='jobs/images', null=True)
+    is_active = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.name
+
+
+class Company(models.Model):
+    name = models.CharField(max_length=100)
+    url = models.CharField(max_length=100)
+
+    class Meta:
+        verbose_name_plural = "Companies"
+
+    def __str__(self):
+        return self.name
+
+
+class Job(models.Model):
+    source = models.ForeignKey(Source, models.PROTECT)
+    company = models.ForeignKey(Company, models.PROTECT)
+    title = models.CharField(max_length=255)
+    desc = models.TextField()
+    url = models.CharField(max_length=255)
+    tech = models.TextField()
+    count = models.IntegerField(default=0)
+    is_active = models.BooleanField(default=True)
+    created = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f'{self.title} @ {self.company}'
+
+    def tech_list(self):
+        return self.tech.split(',') if self.tech else []
+
+
+class Search(models.Model):
+    term = models.CharField(max_length=255)
+    count = models.IntegerField(default=1)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        verbose_name_plural = "Searches"
+
+    def __str__(self):
+        return self.term
